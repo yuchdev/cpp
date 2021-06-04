046822f1d073469d17f19590eaa8ec5188e73f0d
Yurii Cherkasov <yurii.cherkasov@protonmail.com>
2021-06-04 12:28:44 +0700

    Add example of Fast Reverse Sqrt

---
+++ 02_floating_point_types/07_fast_reverse_sqrt/reverse_sqrt.cpp
@@ -0,0 +1,43 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <bitset>
+#include <cmath>
+
+// OsX workaround
+#include <cfloat>
+#include <cstdint>
+
+#include <bitwise.h>
+#include <generate.h>
+
+
+// http://en.wikipedia.org/wiki/Fast_inverse_square_root
+// return 1/sqrt(number)
+float quick_rsqrt(float number)
+{
+	const float x2 = number * 0.5F;
+	const float threehalfs = 1.5F;
+
+	union {
+		float f;
+		uint32_t i;
+	} conv  = { .f = number };
+	conv.i  = 0x5f3759df - ( conv.i >> 1 );
+	conv.f  *= threehalfs - ( x2 * conv.f * conv.f );
+	return conv.f;
+}
+
+
+void benchmark()
+{
+    RandomReal<double> rr;
+    std::vector<double> randoms = rr.gen(10000, 0.0, 10.0);
+}
+
+int main()
+{
+    // TODO: insert any function call
+    return 0;
+}
