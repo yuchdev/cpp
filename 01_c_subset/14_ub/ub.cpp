d7d9d635c7490652d2e9a8726f9606f6fc38bc43
Yurii Cherkasov <yurii.cherkasov@protonmail.com>
2022-04-25 16:13:17 +0700

    Restore 01 from backup

--- 01_c_subset/14_ub/ub.cpp
+++ 01_c_subset/14_ub/ub.cpp
@@ -15,3 +15,10 @@ int main()
 {
     return 0;
 }
+
+// Examples of int/uint UB
+// https://stackoverflow.com/questions/18195715/why-is-unsigned-integer-overflow-defined-behavior-but-signed-integer-overflow-is
+// https://stackoverflow.com/questions/7488837/why-is-int-rather-than-unsigned-int-used-for-c-and-c-for-loops
+// https://stackoverflow.com/questions/199333/how-do-i-detect-unsigned-integer-multiply-overflow
+// https://stackoverflow.com/questions/7488837/why-is-int-rather-than-unsigned-int-used-for-c-and-c-for-loops
+// https://stackoverflow.com/questions/22587451/c-c-use-of-int-or-unsigned-int
