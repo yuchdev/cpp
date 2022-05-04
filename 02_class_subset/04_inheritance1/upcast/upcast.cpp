380bc45193fe59a18dddb5735ce3648953758dcd
Yurii Cherkasov <yurii.cherkasov@protonmail.com>
2022-05-04 13:43:45 +0700

    Upcast&Downcast

--- 02_class_subset/04_inheritance1/upcast/upcast.cpp
+++ 02_class_subset/04_inheritance1/upcast/upcast.cpp
@@ -10,11 +10,13 @@
 #include <functional>
 
 /*
-Up-casting is implicit in C++, and is used a lot when you deal with virtual dispatching. 
+Upcast is conversion of pointer or reference of derived class type to pointer or reference of base class type, going up in the inheritance tree. 
+Upcasting is implicit in C++, and is used a lot when you deal with virtual dispatching. 
 In other words, you have a pointer to Base from which you can access the common interface of a whole hierarchy of classes, 
 and the selection can be done at runtime. This assumes that your interface functions are marked virtual
 
-Down-casting is less useful, and should be avoided whenever one can. 
+Downcast is the conversion of pointer or reference of base class type to pointer or reference type of its derived class, going down in the inheritance tree. 
+Downcasting is less useful, and should be avoided whenever one can. 
 In general is a sign of bad design, as one rarely needs to convert a Base object to a derived one. 
 It can be done (and the result checked) via dynamic_cast
 */
