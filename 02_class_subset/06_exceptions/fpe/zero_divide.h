f0194bb79cb43b83e3e7883afa20b029f15f5741
Yurii Cherkasov <yurii.cherkasov@protonmail.com>
2022-05-04 16:42:12 +0700

    Hide incomplete chapters

--- 02_class_subset/06_exceptions/fpe/zero_divide.h
+++
@@ -1,8 +0,0 @@
-// Демонстрация обработки исключений деления на 0 (целочисленного и FP)
-// Различается на разных платформах
-
-void set_zero_div_exception();
-int zero_div(double s);
-double fpe_zero_div(double s);
-
-
